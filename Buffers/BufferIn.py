04e4e9a247c29801258070e5ea7fc98e38b43347
kbie1 <48415675+kbie1@users.noreply.github.com>
2019-12-19 17:59:10 +0100

    Update BufferIn.py

--- Buffers/BufferIn.py
+++ Buffers/BufferIn.py
@@ -3,16 +3,15 @@ import numpy as np
 
 def createBufferIn(self, data):
 Buffer = np.unpackbits(data).flatten()
-#Jutro dodac pętle działającą aż do opróżnienia bufforu 
 CountIn=1024 #Na przykład 1024, wielkość ramki bez nagłówków
 OffsetIn=0
-for OffsetIn in range (0, len(Buffer):
-DataIn = np.frombuffer(Buffer, dtype='S1', count=CountIn,offset=OffsetIn) 
-if len(DataIn) % 2 != 0:
-            raise Exception('Data is uneven!')
-            ReturnVal = []
-            return ReturnVal
+for OffsetIn in range (0, len(Buffer): #Pętla opróżniająca cały buffer, dodać wyjątek na samą końcówkę
+            DataIn = np.frombuffer(Buffer, dtype='S1', count=CountIn,offset=OffsetIn) 
+                        if len(DataIn) % 2 != 0:
+                                     raise Exception('Data is uneven!')
+                                                ReturnVal = []
+                                                return ReturnVal
 #frame=headers+datain
 #funkcja wysyłająca
-OffsetIn=+CountIn
+            OffsetIn=+CountIn
 
