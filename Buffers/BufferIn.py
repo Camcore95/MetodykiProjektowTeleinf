d54bd5ac171eeeb5ece7992a7372a395cb39ad76
kbie1 <48415675+kbie1@users.noreply.github.com>
2019-12-22 22:18:53 +0100

    Update BufferIn.py

--- Buffers/BufferIn.py
+++ Buffers/BufferIn.py
@@ -1,17 +1,21 @@
 import sys
 import numpy as np
-
-def createBufferIn(self, data):
-Buffer = np.unpackbits(data).flatten()
-CountIn = 1024 #Na przykład 1024, wielkość ramki bez nagłówków
-OffsetIn = 0
-for OffsetIn in range (0, len(Buffer): #Pętla opróżniająca cały buffer, dodać wyjątek na samą końcówkę
-            DataIn = np.frombuffer(Buffer, dtype='S1', count=CountIn,offset=OffsetIn) 
-                        if len(DataIn) % 2 != 0:
-                                     raise Exception('Data is uneven!')
-                                                ReturnVal = []
-                                                return ReturnVal
+testData = np.array([[1,2,3],[4,5,6]],dtype=np.uint8)
+def createBufferIn(data):
+    buffer = np.unpackbits(data).flatten()
+    print(buffer)
+    countIn = 8 #Na przykład 8, wielkość ramki bez nagłówków
+    offsetIn = 0
+    for offsetIn in range (0, len(buffer)-1, countIn): #Pętla opróżniająca cały buffer
+            dataIn = np.frombuffer(buffer, dtype='uint8', count=countIn,offset=offsetIn)
+            if len(dataIn) % 2 != 0:
+                raise Exception('Data is uneven!')
+                returnVal = []
+                return returnVal
 #frame=headers+datain
-#funkcja wysyłająca
-            OffsetIn = OffsetIn + CountIn
+#funkcja wysyłająca, zamiast tego teraz wyświetlanie
+            print("Zawartosc ramki to:",dataIn)
+            offsetIn = offsetIn + countIn
+
+createBufferIn(testData)
 
